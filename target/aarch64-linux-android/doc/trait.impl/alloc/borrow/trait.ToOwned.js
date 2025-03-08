dd8ebaecd654be73e3ae273f1ca392fec24640df
Sebastian Imlay <sebastian.imlay@gmail.com>
2025-03-08 18:19:51 +0000

    Updated docs for latest changes

---
+++ target/aarch64-linux-android/doc/trait.impl/alloc/borrow/trait.ToOwned.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["jni",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"jni/strings/struct.JNIStr.html\" title=\"struct jni::strings::JNIStr\">JNIStr</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[275]}
