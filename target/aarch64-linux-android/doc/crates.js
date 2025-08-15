d2a1ea89b55e7c1bba93f3370b63a9e00ead7543
Sebastian Imlay <sebastian.imlay@gmail.com>
2025-08-15 02:26:12 +0000

    Updated docs for latest changes

---
+++ target/aarch64-linux-android/doc/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["android_bindings","bytes","cesu8","combine","jaffi_support","jni","jni_sys","log","memchr","thiserror"];
+//{"start":21,"fragment_lengths":[18,8,8,10,16,6,10,6,9,12]}
